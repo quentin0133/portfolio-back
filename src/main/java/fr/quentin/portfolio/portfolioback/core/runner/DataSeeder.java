4cc2dc96912aabc88f80f172a56df851d2a1e7b3
Quentin Yahia <quentin.yahia.pro@gmail.com>
2025-05-30 20:15:30 +0200

    create 'files' folder at the beginning :bug:

--- src/main/java/fr/quentin/portfolio/portfolioback/core/runner/DataSeeder.java
+++ src/main/java/fr/quentin/portfolio/portfolioback/core/runner/DataSeeder.java
@@ -3,16 +3,20 @@ package fr.quentin.portfolio.portfolioback.core.runner;
 import fr.quentin.portfolio.portfolioback.user.User;
 import fr.quentin.portfolio.portfolioback.user.UserRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
+
+import java.io.File;
 
 /**
  * The type Data seeder.
  */
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class DataSeeder implements CommandLineRunner {
 
     @Value("${app.default.username}")
@@ -33,6 +37,13 @@ public class DataSeeder implements CommandLineRunner {
             user.getRoles().add("ROLE_ADMIN");
 
             userRepository.save(user);
+        }
+
+        File dir = new File("files");
+        if (!dir.exists()) {
+            boolean created = dir.mkdirs();
+            if(created) log.info("'files' folder created at the root of the project!");
+            else log.error("Impossible to create the ‘files’ folder!");
         }
     }
 }
